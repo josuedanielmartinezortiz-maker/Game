507058858afdee117a5192e6de62b59507703d61
josuedanielmartinezortiz-maker <josuedanielmartinezortiz@gmail.com>
2026-09-05 23:50:09 -0600

    Update play button functionality and scene display

--- game.js
+++ game.js
@@ -1,9 +1,25 @@
+const game = document.getElementById("game");
 const playButton = document.getElementById("playButton");
 
 playButton.addEventListener("click", () => {
 
-    console.log("GAMERPRO GAME: PLAY FUNCIONA");
+    console.log("🎮 GAMERPRO GAME — PLAY");
 
-    alert("¡GAMERPRO GAME INICIADO!");
+    mostrarEscena1();
 
 });
+
+function mostrarEscena1() {
+
+    const inicio = document.getElementById("inicio");
+
+    inicio.style.display = "none";
+
+    const escena = document.createElement("img");
+
+    escena.src = "./escena1.png";
+    escena.id = "escena1";
+
+    game.appendChild(escena);
+
+}
