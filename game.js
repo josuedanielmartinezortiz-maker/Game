7241742a258b90d6f3e64b247a907b0ae62de484
josuedanielmartinezortiz-maker <josuedanielmartinezortiz@gmail.com>
2026-09-05 23:45:37 -0600

    Add play button functionality to game.js

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
+const playButton = document.getElementById("playButton");
 
+playButton.addEventListener("click", () => {
+
+    console.log("GAMERPRO GAME: PLAY FUNCIONA");
+
+    alert("¡GAMERPRO GAME INICIADO!");
+
+});
