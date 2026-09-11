2c40fb1e4cbf959cd4c06702b96ed76d9f519a19
josuedanielmartinezortiz-maker <josuedanielmartinezortiz@gmail.com>
2026-09-10 22:52:29 -0600

    Create torax.js

---
+++ mecanicas/rig/torax.js
@@ -0,0 +1,45 @@
+// =====================================================
+// 🫁 GAMERPRO — RIG DE MICAELA
+// TÓRAX
+// =====================================================
+
+export const HUESOS_TORAX = [
+
+    "esternon",
+
+    "costilla_01_L",
+    "costilla_01_R",
+
+    "costilla_02_L",
+    "costilla_02_R",
+
+    "costilla_03_L",
+    "costilla_03_R",
+
+    "costilla_04_L",
+    "costilla_04_R",
+
+    "costilla_05_L",
+    "costilla_05_R",
+
+    "costilla_06_L",
+    "costilla_06_R",
+
+    "costilla_07_L",
+    "costilla_07_R",
+
+    "costilla_08_L",
+    "costilla_08_R",
+
+    "costilla_09_L",
+    "costilla_09_R",
+
+    "costilla_10_L",
+    "costilla_10_R",
+
+    "costilla_11_L",
+    "costilla_11_R",
+
+    "costilla_12_L",
+    "costilla_12_R"
+];
