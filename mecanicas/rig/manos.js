66c4f828958eb6c0b60d296200f972d534c7d3e0
josuedanielmartinezortiz-maker <josuedanielmartinezortiz@gmail.com>
2026-09-10 22:54:09 -0600

    Add hand bone structure for left and right hands

---
+++ mecanicas/rig/manos.js
@@ -0,0 +1,77 @@
+// =====================================================
+// ✋ GAMERPRO — RIG DE MICAELA
+// MANOS
+// =====================================================
+
+export const HUESOS_MANOS = {
+
+    izquierda: [
+
+        "carpiano_L_01",
+        "carpiano_L_02",
+        "carpiano_L_03",
+        "carpiano_L_04",
+        "carpiano_L_05",
+        "carpiano_L_06",
+        "carpiano_L_07",
+        "carpiano_L_08",
+
+        "metacarpiano_L_01",
+        "metacarpiano_L_02",
+        "metacarpiano_L_03",
+        "metacarpiano_L_04",
+        "metacarpiano_L_05",
+
+        "falange_proximal_L_01",
+        "falange_proximal_L_02",
+        "falange_proximal_L_03",
+        "falange_proximal_L_04",
+        "falange_proximal_L_05",
+
+        "falange_media_L_02",
+        "falange_media_L_03",
+        "falange_media_L_04",
+        "falange_media_L_05",
+
+        "falange_distal_L_01",
+        "falange_distal_L_02",
+        "falange_distal_L_03",
+        "falange_distal_L_04",
+        "falange_distal_L_05"
+    ],
+
+    derecha: [
+
+        "carpiano_R_01",
+        "carpiano_R_02",
+        "carpiano_R_03",
+        "carpiano_R_04",
+        "carpiano_R_05",
+        "carpiano_R_06",
+        "carpiano_R_07",
+        "carpiano_R_08",
+
+        "metacarpiano_R_01",
+        "metacarpiano_R_02",
+        "metacarpiano_R_03",
+        "metacarpiano_R_04",
+        "metacarpiano_R_05",
+
+        "falange_proximal_R_01",
+        "falange_proximal_R_02",
+        "falange_proximal_R_03",
+        "falange_proximal_R_04",
+        "falange_proximal_R_05",
+
+        "falange_media_R_02",
+        "falange_media_R_03",
+        "falange_media_R_04",
+        "falange_media_R_05",
+
+        "falange_distal_R_01",
+        "falange_distal_R_02",
+        "falange_distal_R_03",
+        "falange_distal_R_04",
+        "falange_distal_R_05"
+    ]
+};
